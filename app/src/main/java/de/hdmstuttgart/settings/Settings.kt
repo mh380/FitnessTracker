77dfa2b79cbc6966e033257df59a24f8acfc7785
Karin <kd053@hdm-stuttgart.de>
2023-12-07 12:19:07 +0100

    I don't know how to implement the toggle

--- app/src/main/java/de/hdmstuttgart/settings/Settings.kt
+++ app/src/main/java/de/hdmstuttgart/settings/Settings.kt
@@ -1,13 +1,12 @@
 package de.hdmstuttgart.settings
 
+import androidx.recyclerview.widget.RecyclerView
+import de.hdmstuttgart.trackmaster.R
+
+
 class Settings {
 
-    // toggle between light and dark mode
-
-
-
-
-
+    // todo: toggle between light and dark mode
 
 
 
