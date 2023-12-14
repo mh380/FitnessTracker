60e72715da8f729f7f880071ecf5f208582fb4f1
Marvin Helmer <mh380@hdm-stuttgart.de>
2023-12-14 10:24:25 +0100

    implementing google maps in NewActivity

--- app/src/main/java/de/hdmstuttgart/trackmaster/homeScreen/MainActivity.kt
+++ app/src/main/java/de/hdmstuttgart/trackmaster/homeScreen/MainActivity.kt
@@ -1,29 +1,18 @@
 package de.hdmstuttgart.trackmaster.homeScreen
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
-import com.google.android.gms.maps.CameraUpdateFactory
-import com.google.android.gms.maps.GoogleMap
-import com.google.android.gms.maps.OnMapReadyCallback
-import com.google.android.gms.maps.SupportMapFragment
-import com.google.android.gms.maps.model.LatLng
-import com.google.android.gms.maps.model.MarkerOptions
+import androidx.appcompat.app.AppCompatActivity
 import de.hdmstuttgart.settings.SettingsActivity
-import de.hdmstuttgart.trackmaster.newActivityScreen.NewActivity
 import de.hdmstuttgart.trackmaster.R
+import de.hdmstuttgart.trackmaster.newActivityScreen.NewActivity
 
-class MainActivity : AppCompatActivity(), OnMapReadyCallback {
-
-    private lateinit var googleMap: GoogleMap
+class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        val mapFragment = supportFragmentManager.findFragmentById(R.id.map) as SupportMapFragment
-        mapFragment.getMapAsync(this)
     }
 
     fun onFloatingActionButtonClick(view: View) {
@@ -34,17 +23,5 @@ class MainActivity : AppCompatActivity(), OnMapReadyCallback {
     fun onSettingsButtonClick (view: View) {
         val intent = Intent(this, SettingsActivity::class.java)
         startActivity(intent)
-    }
-
-    override fun onMapReady(p0: GoogleMap) {
-
-        googleMap = p0
-
-        //Coordinates of a specific location you can change if you want another location
-        val location = LatLng(48.7823200, 9.1770200);
-        googleMap.addMarker(
-            MarkerOptions().position(location).title("Marker in Stuttgart")
-        )
-        googleMap.moveCamera(CameraUpdateFactory.newLatLng(location))
     }
 }
--- app/src/main/java/de/hdmstuttgart/trackmaster/newActivityScreen/NewActivity.kt
+++ app/src/main/java/de/hdmstuttgart/trackmaster/newActivityScreen/NewActivity.kt
@@ -2,11 +2,35 @@ package de.hdmstuttgart.trackmaster.newActivityScreen
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.google.android.gms.maps.CameraUpdateFactory
+import com.google.android.gms.maps.GoogleMap
+import com.google.android.gms.maps.OnMapReadyCallback
+import com.google.android.gms.maps.SupportMapFragment
+import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.maps.model.MarkerOptions
 import de.hdmstuttgart.trackmaster.R
 
-class NewActivity : AppCompatActivity() {
+class NewActivity : AppCompatActivity(), OnMapReadyCallback {
+
+    private lateinit var googleMap: GoogleMap
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_new)
+
+        val mapFragment = supportFragmentManager.findFragmentById(R.id.map) as SupportMapFragment
+        mapFragment.getMapAsync(this)
+    }
+
+    override fun onMapReady(p0: GoogleMap) {
+
+        googleMap = p0
+
+        //Coordinates of a specific location you can change if you want another location
+        val location = LatLng(48.7823200, 9.1770200);
+        googleMap.addMarker(
+            MarkerOptions().position(location).title("Marker in Stuttgart")
+        )
+        googleMap.moveCamera(CameraUpdateFactory.newLatLng(location))
     }
 }
