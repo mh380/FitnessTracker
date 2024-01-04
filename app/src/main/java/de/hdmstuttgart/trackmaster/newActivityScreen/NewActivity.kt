0c3555ccf99d52300980cb17549eab8e48769efb
Marvin Helmer <mh380@hdm-stuttgart.de>
2024-01-04 19:20:20 +0100

    NewActivity rewritten.

--- app/src/main/java/de/hdmstuttgart/trackmaster/newActivityScreen/NewActivity.kt
+++ app/src/main/java/de/hdmstuttgart/trackmaster/newActivityScreen/NewActivity.kt
@@ -1,101 +1,19 @@
 package de.hdmstuttgart.trackmaster.newActivityScreen
 
-import android.content.pm.PackageManager
-import android.location.Location
 import android.os.Bundle
-import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.app.ActivityCompat
-import androidx.core.content.ContextCompat
-import com.google.android.gms.location.FusedLocationProviderClient
-import com.google.android.gms.location.LocationServices
-import com.google.android.gms.maps.CameraUpdateFactory
-import com.google.android.gms.maps.GoogleMap
-import com.google.android.gms.maps.OnMapReadyCallback
-import com.google.android.gms.maps.SupportMapFragment
-import com.google.android.gms.maps.model.LatLng
-import com.google.android.gms.maps.model.MarkerOptions
 import de.hdmstuttgart.trackmaster.R
 
-class NewActivity : AppCompatActivity(), OnMapReadyCallback {
-
-    private val FINE_LOCATION_PERMISSION_CODE = 1
-    private lateinit var googleMap: GoogleMap
-    private var currentLocation: Location? = null
-    private lateinit var fusedLocationClient: FusedLocationProviderClient
+class NewActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_new)
-        fusedLocationClient = LocationServices.getFusedLocationProviderClient(this)
-        if (checkLocationPermission()) {
-            getLastLocation()
-        } else {
-            requestLocationPermission()
-        }
-    }
 
-    private fun checkLocationPermission(): Boolean {
-        return ContextCompat.checkSelfPermission(
-            this,
-            android.Manifest.permission.ACCESS_FINE_LOCATION
-        ) == PackageManager.PERMISSION_GRANTED
-    }
-
-    private fun requestLocationPermission() {
-        ActivityCompat.requestPermissions(
-            this,
-            arrayOf(android.Manifest.permission.ACCESS_FINE_LOCATION),
-            FINE_LOCATION_PERMISSION_CODE
-        )
-    }
-
-    private fun initializeMap() {
-        val mapFragment = supportFragmentManager.findFragmentById(R.id.map) as SupportMapFragment
-        mapFragment.getMapAsync(this@NewActivity)
-    }
-
-    private fun getLastLocation() {
-        if (checkLocationPermission()) {
-            fusedLocationClient.lastLocation.addOnSuccessListener { location: Location? ->
-                location?.let {
-                    currentLocation = it
-                    initializeMap()
-                }
-            }
-        } else {
-            requestLocationPermission()
-        }
-    }
-
-    override fun onMapReady(googleMap: GoogleMap) {
-        this.googleMap = googleMap
-
-        // Coordinates of the current location, change if needed
-        val location = currentLocation?.let { LatLng(it.latitude, it.longitude) }
-        location?.let {
-            googleMap.addMarker(MarkerOptions().position(it).title("My Location"))
-            googleMap.moveCamera(CameraUpdateFactory.newLatLng(it))
-        }
-    }
-
-    override fun onRequestPermissionsResult(
-        requestCode: Int,
-        permissions: Array<out String>,
-        grantResults: IntArray
-    ) {
-        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
-        if (requestCode == FINE_LOCATION_PERMISSION_CODE &&
-            grantResults.isNotEmpty() && grantResults[0] == PackageManager.PERMISSION_GRANTED
-        ) {
-            // Location permission granted, you can perform actions here if needed
-            getLastLocation()
-        } else {
-            Toast.makeText(
-                this,
-                "Location permission is denied, please allow the permission",
-                Toast.LENGTH_SHORT
-            ).show()
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.fragment_container, MapsFragment())
+                .commit()
         }
     }
 }
