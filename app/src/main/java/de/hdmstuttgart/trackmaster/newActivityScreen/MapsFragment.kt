1f7c560bc3ab7ad98fd1d42428f7f9febd3a04fb
Marvin Helmer <mh380@hdm-stuttgart.de>
2024-01-20 00:20:04 +0100

    xml Layout angepasst, Zoom Funktionen hinzugefügt, Search View entfernt

--- app/src/main/java/de/hdmstuttgart/trackmaster/newActivityScreen/MapsFragment.kt
+++ app/src/main/java/de/hdmstuttgart/trackmaster/newActivityScreen/MapsFragment.kt
@@ -1,14 +1,12 @@
 package de.hdmstuttgart.trackmaster.newActivityScreen
 
 import android.content.pm.PackageManager
-import android.location.Address
-import android.location.Geocoder
 import android.location.Location
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.SearchView
+import android.widget.Toast
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 import androidx.fragment.app.Fragment
@@ -21,7 +19,6 @@ import com.google.android.gms.maps.SupportMapFragment
 import com.google.android.gms.maps.model.LatLng
 import com.google.android.gms.maps.model.MarkerOptions
 import de.hdmstuttgart.trackmaster.R
-import java.io.IOException
 
 class MapsFragment : Fragment(), OnMapReadyCallback {
 
@@ -29,7 +26,6 @@ class MapsFragment : Fragment(), OnMapReadyCallback {
     private lateinit var googleMap: GoogleMap
     private var currentLocation: Location? = null
     private lateinit var fusedLocationClient: FusedLocationProviderClient
-    private lateinit var mapSearchView: SearchView
 
     override fun onCreateView(
         inflater: LayoutInflater,
@@ -41,53 +37,13 @@ class MapsFragment : Fragment(), OnMapReadyCallback {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-
         fusedLocationClient = LocationServices.getFusedLocationProviderClient(requireActivity())
-        mapSearchView = view.findViewById(R.id.mapSearch)
 
         if (checkLocationPermission()) {
             getLastLocation()
         } else {
             requestLocationPermission()
         }
-
-
-        mapSearchView.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
-            override fun onQueryTextSubmit(query: String?): Boolean {
-                val location = mapSearchView.query.toString()
-                var addressList: List<Address>? = null
-
-                val geocoder = Geocoder(requireContext())
-
-                try {
-                    addressList = geocoder.getFromLocationName(location, 1)
-                } catch (e: IOException) {
-                    e.printStackTrace()
-                }
-
-                if (!addressList.isNullOrEmpty()) {
-                    val address = addressList[0]
-                    val latLng = LatLng(address.latitude, address.longitude)
-
-                    // Move the camera smoothly to the searched location
-                    googleMap.animateCamera(CameraUpdateFactory.newLatLngZoom(latLng, 1f))
-
-                    // Add a marker for the searched location
-                    googleMap.addMarker(MarkerOptions().position(latLng).title("Searched Location"))
-                }
-
-                return false
-            }
-
-            override fun onQueryTextChange(newText: String?): Boolean {
-                // Handle the query text change if needed
-                return false
-            }
-        });
-
-        val mapFragment = childFragmentManager.findFragmentById(R.id.map) as SupportMapFragment?
-        mapFragment?.getMapAsync(this)
-
     }
 
     private fun checkLocationPermission(): Boolean {
@@ -105,32 +61,50 @@ class MapsFragment : Fragment(), OnMapReadyCallback {
         )
     }
 
-    private fun initializeMap() {
-        val mapFragment = childFragmentManager.findFragmentById(R.id.map) as SupportMapFragment
-        mapFragment.getMapAsync(this@MapsFragment)
-    }
-
     private fun getLastLocation() {
         if (checkLocationPermission()) {
-            fusedLocationClient.lastLocation.addOnSuccessListener { location: Location? ->
-                location?.let {
-                    currentLocation = it
-                    initializeMap()
+            fusedLocationClient.lastLocation
+                .addOnSuccessListener { location: Location? ->
+                    location?.let {
+                        currentLocation = it
+                        initializeMap()
+                    } ?: run {
+                        showErrorMessage("Unable to get location. Please check your settings.")
+                    }
                 }
-            }
+                .addOnFailureListener { e ->
+                    showErrorMessage("Error getting location: ${e.localizedMessage}")
+                }
         } else {
             requestLocationPermission()
+        }
+    }
+
+    private fun initializeMap() {
+        if (isAdded) {
+            val mapFragment =
+                childFragmentManager.findFragmentById(R.id.map) as SupportMapFragment
+            mapFragment.getMapAsync(this@MapsFragment)
         }
     }
 
     override fun onMapReady(googleMap: GoogleMap) {
         this.googleMap = googleMap
 
-        // Coordinates of the current location, change if needed
         val location = currentLocation?.let { LatLng(it.latitude, it.longitude) }
         location?.let {
             googleMap.addMarker(MarkerOptions().position(it).title("My Location"))
-            googleMap.moveCamera(CameraUpdateFactory.newLatLng(it))
+            googleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(it, 5f))
+
+            googleMap.uiSettings.isRotateGesturesEnabled = true;
+            googleMap.uiSettings.isRotateGesturesEnabled = true;
+
+
+
         }
+    }
+
+    private fun showErrorMessage(message: String) {
+        Toast.makeText(requireContext(), message, Toast.LENGTH_SHORT).show()
     }
 }
