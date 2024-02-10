a64aaf099a80248dc46a72feadbe5b4689bb89fa
Marvin Helmer <mh380@hdm-stuttgart.de>
2024-02-10 17:13:45 +0100

    Setting up Map

--- app/src/main/java/de/hdmstuttgart/trackmaster/services/TrackingService.kt
+++ app/src/main/java/de/hdmstuttgart/trackmaster/services/TrackingService.kt
@@ -22,6 +22,7 @@ import com.google.android.gms.location.LocationServices
 import com.google.android.gms.location.Priority.PRIORITY_HIGH_ACCURACY
 import com.google.android.gms.maps.model.LatLng
 import de.hdmstuttgart.trackmaster.R
+import de.hdmstuttgart.trackmaster.newActivityScreen.NewActivity
 import de.hdmstuttgart.trackmaster.utils.Constants
 import de.hdmstuttgart.trackmaster.utils.Constants.ACTION_PAUSE_SERVICE
 import de.hdmstuttgart.trackmaster.utils.Constants.ACTION_START_OR_RESUME_SERVICE
@@ -43,18 +44,12 @@ typealias Polylines = MutableList<Polyline>
 //@AndroidEntryPoint
 class TrackingService : LifecycleService() {
 
-    var isFirstRun = true
+    private var isFirstRun = true
 
-    //@Inject
-    lateinit var fusedLocationProviderClient: FusedLocationProviderClient
+    private lateinit var fusedLocationProviderClient: FusedLocationProviderClient
 
     //Implementing the Stop Watch
     private val timeRunInSeconds = MutableLiveData<Long>()
-
-    //@Inject
-    lateinit var baseNotificationBuilder: NotificationCompat.Builder
-
-    lateinit var curNotificationBuilder: NotificationCompat.Builder
 
     //Whenever the tracking state changes post the new value inside isTracking
     companion object {
@@ -72,13 +67,11 @@ class TrackingService : LifecycleService() {
 
     override fun onCreate() {
         super.onCreate()
-        curNotificationBuilder = baseNotificationBuilder
         postInitialValues()
         fusedLocationProviderClient = LocationServices.getFusedLocationProviderClient(this)
 
         isTracking.observe(this, Observer {
             updateLocationTracking(it)
-            updateNotificationTrackingState(it)
         })
     }
 
@@ -87,7 +80,6 @@ class TrackingService : LifecycleService() {
             when(it.action) {
                 ACTION_START_OR_RESUME_SERVICE -> {
                     if(isFirstRun) {
-                        startForegroundService()
                         isFirstRun = false
                     } else {
                         Log.d("TrackingService", "Resumed service")
@@ -114,7 +106,7 @@ class TrackingService : LifecycleService() {
 
     //Implementing the Stop Watch
 
-    var isTimerEnabled = false
+    private var isTimerEnabled = false
     private var lapTime = 0L
     private var timeRun = 0L
     private var timeStarted = 0L
@@ -147,52 +139,26 @@ class TrackingService : LifecycleService() {
     }
 
     // Implementing the notification tracking state (damit updaten wir die Notification)
-    private fun updateNotificationTrackingState(isTracking: Boolean) {
-        val notificationActionText = if(isTracking) "Pause" else "Resume"
-        val pendingIntent = if(isTracking) {
-            val pauseIntent = Intent(this, TrackingService::class.java).apply {
-                action = ACTION_PAUSE_SERVICE
-            }
-            PendingIntent.getService(this, 1, pauseIntent, PendingIntent.FLAG_UPDATE_CURRENT)
-        } else {
-            val resumeIntent = Intent(this, TrackingService::class.java).apply {
-                action = ACTION_START_OR_RESUME_SERVICE
-            }
-            PendingIntent.getService(this, 2, resumeIntent, PendingIntent.FLAG_UPDATE_CURRENT)
-        }
 
-        val notificationManager = getSystemService(NOTIFICATION_SERVICE) as NotificationManager
-
-        curNotificationBuilder.javaClass.getDeclaredField("mActions").apply {
-            isAccessible = true
-            set(curNotificationBuilder, ArrayList<NotificationCompat.Action>())
-        }
-        curNotificationBuilder = baseNotificationBuilder
-            .addAction(R.drawable.ic_launcher_foreground, notificationActionText, pendingIntent)
-        notificationManager.notify(Constants.NOTIFICATION_ID, curNotificationBuilder.build())
-    }
 
     @SuppressLint("MissingPermission")
     private fun updateLocationTracking(isTracking: Boolean) {
-        if(isTracking) {
-            if (true) { //TrackingUtility.hasLocationPermissions(this)) { //todo: fix this to check the permissions
-                val request = LocationRequest().apply {
-                    interval = LOCATION_UPDATE_INTERVAL
-                    fastestInterval = FASTEST_LOCATION_INTERVAL
-                    priority = PRIORITY_HIGH_ACCURACY
-                }
-                fusedLocationProviderClient.requestLocationUpdates(
-                    request,
-                    locationCallback,
-                    Looper.getMainLooper()
-                )
+        if (isTracking && TrackingUtility.hasLocationPermissions(this)) {
+            val request = LocationRequest().apply {
+                interval = LOCATION_UPDATE_INTERVAL
+                fastestInterval = FASTEST_LOCATION_INTERVAL
+                priority = PRIORITY_HIGH_ACCURACY
             }
+            fusedLocationProviderClient.requestLocationUpdates(
+                request,
+                locationCallback,
+                Looper.getMainLooper()
+            )
         } else {
             fusedLocationProviderClient.removeLocationUpdates(locationCallback)
         }
     }
-
-    val locationCallback = object : LocationCallback() {
+    private val locationCallback = object : LocationCallback() {
         override fun onLocationResult(p0: LocationResult) {
             super.onLocationResult(p0)
             if(isTracking.value!!) {
@@ -221,52 +187,6 @@ class TrackingService : LifecycleService() {
         pathPoints.postValue(this)
     } ?: pathPoints.postValue(mutableListOf(mutableListOf()))
 
-    private fun startForegroundService() {
-        startTimer()
-        isTracking.postValue(true)
 
-        val notificationManager = getSystemService(NOTIFICATION_SERVICE) as NotificationManager
-
-        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-            createNotificationChannel(notificationManager)
-        }
-
-        // nicht mehr benötigt da wir jetzt Hilt verwenden!!!
-        /*val notificationBuilder = NotificationCompat.Builder(this, Constants.NOTIFICATION_CHANNEL_ID)
-            .setAutoCancel(false)
-            .setOngoing(true)
-            .setSmallIcon(R.drawable.ic_launcher_foreground)
-            .setContentTitle("TrackMaster")
-            .setContentText("00:00:00")
-            .setContentIntent(getMainActivityPendingIntent())*/
-
-
-
-        startForeground(Constants.NOTIFICATION_ID, baseNotificationBuilder.build())
-        timeRunInSeconds.observe(this, Observer {
-            val notification = curNotificationBuilder
-                .setContentText(TrackingUtility.getFormattedStopWatchTime(it * 1000L))
-            notificationManager.notify(Constants.NOTIFICATION_ID, notification.build())
-        })
-    }
-
-    // nicht mehr benötigt da wir jetzt Hilt verwenden!!!
-    /*private fun getMainActivityPendingIntent() = PendingIntent.getActivity(
-        this,
-        0,
-        Intent(this, MainActivity::class.java).also {
-            it.action = Constants.ACTION_SHOW_TRACKING_FRAGMENT
-        },
-        PendingIntent.FLAG_UPDATE_CURRENT
-    )*/
-
-    private fun createNotificationChannel(notificationManager: NotificationManager) {
-        val channel = NotificationChannel(
-            Constants.NOTIFICATION_CHANNEL_ID,
-            Constants.NOTIFICATION_CHANNEL_NAME,
-            IMPORTANCE_LOW
-        )
-        notificationManager.createNotificationChannel(channel)
-    }
 }
 
--- app/src/main/java/de/hdmstuttgart/trackmaster/utils/TrackingUtility.kt
+++ app/src/main/java/de/hdmstuttgart/trackmaster/utils/TrackingUtility.kt
@@ -1,27 +1,39 @@
 package de.hdmstuttgart.trackmaster.utils
 
+import android.content.Context
+import android.content.pm.PackageManager
+import android.os.Build
+import androidx.core.content.ContextCompat
 import de.hdmstuttgart.trackmaster.services.Polyline
 import java.util.concurrent.TimeUnit
 
 object TrackingUtility {
 
-    /*
-    fun hasLocationPermissions(context: Context) =
-        if(Build.VERSION.SDK_INT < Build.VERSION_CODES.Q) {
-            EasyPermissions.hasPermissions(
+    fun hasLocationPermissions(context: Context): Boolean {
+        return if (Build.VERSION.SDK_INT < Build.VERSION_CODES.Q) {
+            ContextCompat.checkSelfPermission(
                 context,
-                android.Manifest.permission.ACCESS_FINE_LOCATION,
-                android.Manifest.permission.ACCESS_COARSE_LOCATION
-            )
+                android.Manifest.permission.ACCESS_FINE_LOCATION
+            ) == PackageManager.PERMISSION_GRANTED &&
+                    ContextCompat.checkSelfPermission(
+                        context,
+                        android.Manifest.permission.ACCESS_COARSE_LOCATION
+                    ) == PackageManager.PERMISSION_GRANTED
         } else {
-            EasyPermissions.hasPermissions(
+            ContextCompat.checkSelfPermission(
                 context,
-                android.Manifest.permission.ACCESS_FINE_LOCATION,
-                android.Manifest.permission.ACCESS_COARSE_LOCATION,
-                android.Manifest.permission.ACCESS_BACKGROUND_LOCATION
-            )
+                android.Manifest.permission.ACCESS_FINE_LOCATION
+            ) == PackageManager.PERMISSION_GRANTED &&
+                    ContextCompat.checkSelfPermission(
+                        context,
+                        android.Manifest.permission.ACCESS_COARSE_LOCATION
+                    ) == PackageManager.PERMISSION_GRANTED &&
+                    ContextCompat.checkSelfPermission(
+                        context,
+                        android.Manifest.permission.ACCESS_BACKGROUND_LOCATION
+                    ) == PackageManager.PERMISSION_GRANTED
         }
-     */
+    }
 
     //Berechnet die Länge der Polyline
     fun calculatePolylineLength(polyline: Polyline): Float {
